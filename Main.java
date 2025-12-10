d14f7441cb347baeab7819adddad00f40c7a71b7
Trevin Perera <trevinperera04@gmail.com>
2025-12-10 16:36:25 +0530

    Create Service

--- Main.java
+++ Main.java
@@ -26,3 +26,38 @@ class Customer {
         return id + " - " + name + " (" + phone + ")";
     }
 }
+
+class Service {
+    private int id;
+    private String name;
+    private double price;
+    private int durationMinutes;
+
+    public Service(int id, String name, double price, int durationMinutes) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.durationMinutes = durationMinutes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getDurationMinutes() {
+        return durationMinutes;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + name + " | Rs. " + price + " | " + durationMinutes + " mins";
+    }
+}
